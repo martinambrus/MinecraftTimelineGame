ba8ac06e6ffa276d2e27486ef9fac3d5edb26bf6
Martin Ambrus <martinambrus@users.noreply.github.com>
2025-10-21 17:38:45 +0200

    Provide headless GL stub for tests

--- core/src/test/java/com/minecrafttimeline/TestApplicationSupport.java
+++ core/src/test/java/com/minecrafttimeline/TestApplicationSupport.java
@@ -1,9 +1,12 @@
 package com.minecrafttimeline;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.utils.GdxNativesLoader;
+import com.minecrafttimeline.headless.HeadlessMockGL20;
 
 /**
  * Utility used by tests to bootstrap the LibGDX headless environment once.
@@ -28,5 +31,8 @@ public final class TestApplicationSupport {
         application = new HeadlessApplication(new ApplicationAdapter() {
             // no implementation required for tests
         }, configuration);
+        final GL20 mockGL20 = new HeadlessMockGL20();
+        Gdx.gl20 = mockGL20;
+        Gdx.gl = mockGL20;
     }
 }
